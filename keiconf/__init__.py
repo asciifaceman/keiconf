05fedbf50ddca2bd324904468430654236c40d44
Charles Corbett <nafredy@gmail.com>
2023-04-21 11:12:30 -0700

    pushing cause I have to gtfo and dont want to forget

--- keiconf/__init__.py
+++ keiconf/__init__.py
@@ -1,3 +1,21 @@
 # SPDX-FileCopyrightText: 2023-present Charles C. <nafredy@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+
+from pathlib import Path
+
+class KeiConf:
+    '''
+    A simple and minimalist flat file configuration for small projects and prototyping
+    '''
+
+    def __init__(self, filepath: Path = None):
+        '''
+        Initialize the configuration manager
+        '''
+
+        if not isinstance(filepath, Path):
+            raise TypeError(f"Expected filepath to be PATH but got {type(filepath)}: {filepath}")
+
+        self._filepath = filepath
